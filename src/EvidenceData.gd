a86ac92ab787127bc8f2ad755b7404b39ed23684
KvaGram <0kvakk@gmailcom>
2022-02-02 02:30:09 +0100

    finish coding the UI
    Minor glitches left to fix before publishing.

--- src/EvidenceData.gd
+++ src/EvidenceData.gd
@@ -3,5 +3,5 @@ class_name EvidenceData
 
 export(String) var name
 export(String, MULTILINE) var description
-export(Texture) var icon
-export(Array, Texture) var fullscreens
+export(StreamTexture) var icon
+export(Array, StreamTexture) var fullscreens
--- src/court_record.gd
+++ src/court_record.gd
@@ -4,10 +4,15 @@ var itemrow:Array
 #data. Ordered by case, episode then part.
 #The part resource is a RecordData
 export(Array, Array, Array, Resource) var data
+export(StreamTexture) var evidence_background
+export(StreamTexture) var profile_background
+export(StreamTexture) var empty_item
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var recordData:RecordData
+var itempage := 0
+var profiles_mode := false
+var detailspage := 0
+var s_item := 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,8 +29,10 @@ func _ready():
 		$itemrow/items/item8,
 		$itemrow/items/item9,
 	]
-	for i in itemrow:
-		(i as TextureButton).connect("pressed", self, "on_item_selected", [i])
+	for i in len(itemrow):
+		var item:TextureButton = itemrow[i]
+# warning-ignore:return_value_discarded
+		item.connect("pressed", self, "on_item_selected", [i])
 	
 	var pop_version:PopupMenu = $btn_versions.get_popup()
 	var case_i := 0
@@ -50,16 +57,102 @@ func _ready():
 			ep_i += 1 
 		case_i += 1 
 	#end of version select setup
+	on_part_selected(0,0,0)
 		
 func on_part_selected(part_i, case_i, ep_i):
 	print("part_i " + str(part_i) + " - case_i " + str(case_i) + " - ep_i " + str(ep_i) )
-
-func on_item_selected(index:int):
-	print("item selected: " + str(index))
-	pass
-
+	$btn_versions.text = "Case %s episode %s, part %s" % [case_i+1, ep_i+1, part_i]
+	recordData = data[case_i][ep_i][part_i]
+	refresh()
+func refresh():
+	$img_background.texture = profile_background if profiles_mode else evidence_background
+	var itemlist:Array = recordData.profiles if profiles_mode else recordData.evidence
+	#sanitize the itempage
+	itempage = clamp(itempage, 0, int((len(itemlist)-1)/10))
+	for i in range(10):
+		var item_i = i + 10*itempage
+		var item_btn:TextureButton = itemrow[i]
+		if(item_i >= len(itemlist)):
+			item_btn.disabled = true
+			item_btn.texture_normal = empty_item
+		else:
+			item_btn.disabled = false
+			item_btn.texture_normal = itemlist[item_i].icon
+		$itemrow/PREV.disabled = true if itempage <= 0 else false
+		$itemrow/NEXT.disabled = len(itemlist) <= ((itempage+1) * 10)
+	on_item_selected(0)
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func on_item_selected(index:int):
+	s_item = index + 10*itempage
+	var itemlist:Array = recordData.profiles if profiles_mode else recordData.evidence
+	if(s_item >= len(itemlist)):
+		$txt_title.text = ""
+		$txt_details.text = ""
+		$img_item.texture = empty_item
+		$btn_details.disabled = true
+		$btn_details.visible = false
+	else:
+		var edata:EvidenceData = itemlist[s_item]
+		if(len(edata.fullscreens) <= 0):
+			$btn_details.disabled = true
+			$btn_details.visible = false
+		else:
+			$btn_details.disabled = false
+			$btn_details.visible = true
+		$txt_title.text = edata.name
+		$txt_details.text = edata.description
+		$img_item.texture = edata.icon	
+
+func refresh_details():
+	var itemlist:Array = recordData.profiles if profiles_mode else recordData.evidence
+	if(s_item >= len(itemlist)):
+		$pop_fullscreens.hide() #contingency. Hide if selected item does not exists
+		return
+	var edata:EvidenceData = itemlist[s_item]
+	if(len(edata.fullscreens) <=0):
+		$pop_fullscreens.hide() #contingency. Hide if selected item does have details to show
+		return
+	detailspage = clamp(detailspage, 0, len(edata.fullscreens)-1)
+	$"pop_fullscreens/row/d-NEXT".visible = (len(edata.fullscreens) > 1)
+	$"pop_fullscreens/row/d-PREV".visible = (len(edata.fullscreens) > 1)
+	$"pop_fullscreens/row/d-NEXT".disabled = (detailspage >= len(edata.fullscreens)-1)
+	$"pop_fullscreens/row/d-PREV".disabled = detailspage <= 0
+	
+	$pop_fullscreens/row/content.texture = edata.fullscreens[detailspage]
+	
+	
+
+
+func _on_PREV_pressed():
+	itempage-=1 #value is sanitized in refresh
+	refresh()
+func _on_NEXT_pressed():
+	itempage+=1 #value is sanitized in refresh
+	refresh()
+
+
+func _on_dPREV_pressed():
+	detailspage -= 1
+	refresh_details()
+
+
+func _on_dNEXT_pressed():
+	detailspage += 1
+	refresh_details()
+
+#summoms the large pictures in a large popup
+func _on_btn_details_pressed():
+	detailspage = 0
+	$pop_fullscreens.popup_centered()
+	refresh_details()
+
+
+
+#switch between evidence and profiles
+func _on_btn_switch_pressed():
+	profiles_mode = not profiles_mode
+	$btn_switch.text = "Evidence" if profiles_mode else "Profiles"
+	$txt_group.text = "Profiles" if profiles_mode else "Evidence"
+	refresh()
+	
